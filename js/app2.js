6d4064bd1a7723c713829a3e20495964a1af0cde
Yann Merchier <yann.merchier@gmail.com>
2019-04-28 19:19:59 +0200

    bottle animation

--- js/app2.js
+++ js/app2.js
@@ -1,17 +1,48 @@
 /*jslint browser: true*/
 /*global $, jQuery*/
 $( document ).ready(function() {
-    $('.bottle1').animate({
-        left: '5em'
-    }, 2350);
-    $('.bottle2').animate({
-        left: '5em'
-    }, 2450);
-    $('.bottle3').animate({
-        left: '5em'
-    }, 2500);
-    // $('.desc').animate({opacity:1}, 1500);
-    // $('.desc').animate({bottom:'1em'}, 1500);
+    $(window).scroll(function() {
+   var hT = $('#scroll-to-red').offset().top,
+       hH = $('#scroll-to-red').outerHeight(),
+       wH = $(window).height(),
+       wS = $(this).scrollTop();
+    console.log((hT-wH) , wS);
+   if (wS > (hT+hH-wH)){
+       $('.Redbottle1').animate({
+               left: '5em'
+           }, 2000);
+       $('.Redbottle2').animate({
+           left: '5em'
+       }, 2100);
+       $('.Redbottle3').animate({
+           left: '5em'
+       }, 2150);
+       $('.red_desc').animate({
+           bottom: "1em", opacity: 1
+       }, { duration: 500, queue: false })
+   }
+    });
+    $(window).scroll(function() {
+   var hT = $('#scroll-to-white').offset().top,
+       hH = $('#scroll-to-white').outerHeight(),
+       wH = $(window).height(),
+       wS = $(this).scrollTop();
+    console.log((hT-wH) , wS);
+   if (wS > (hT+hH-wH)){
+       $('.Whitebottle1').animate({
+               right: '-3em'
+           }, 2000);
+       $('.Whitebottle2').animate({
+           right: '-3em'
+       }, 2100);
+       $('.Whitebottle3').animate({
+           right: '-3em'
+       }, 2150);
+       $('.white_desc').animate({
+           bottom: "1em", opacity: 1
+       }, { duration: 500, queue: false })
+   }
+    });
     $('.desc').animate({
         bottom: "1em", opacity: 1
     }, { duration: 2500, queue: false })
